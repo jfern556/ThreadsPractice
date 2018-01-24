fab837b974d269dfdd32115da736afb870336fc0
Christian Hidalgo <chidalgo088@gmail.com>
2018-01-23 19:35:14 -0500

    added a validation
    Also, i added PROTOTYPES for the functions for the threads (look at function "say_hello"-- that one is correct)

--- threads.c
+++ threads.c
@@ -4,22 +4,62 @@
 #include <stdlib.h>
 
 
-
-void SimpleThread(int);
-
+//-------------------
+void* SimpleThread(int);
+void* say_hello();
+int validateInput(char, int);
+//-------------------
 int SharedVariable = 0; 
+//-------------------
 
 int main( int argc, char *argv[]){
 	
-	printf("*** The Program has Started\n");
+	printf("*******************************\n");
+	printf("*** The Program has Started ***\n");
+	printf("*******************************\n");
 
-	int SharedVariable = 0; 
+	char c;
+	c = (*argv[1]);
+
+	int result;
+	
+	result = validateInput(c, argc);
+
+	printf("this is argc: %d and this is the number: %d\n" ,argc , result);
+
+	pthread_t myThread;
+
+	pthread_create(&myThread, NULL, say_hello, "Hello" );
+
+	pthread_join(myThread, NULL);
 
 	
 		
 }
 
-void SimpleThread (int which){
+//if you add 0 to a char, it basically returns its ASCII value.
+//so if we do char - 48 we get the actual digit from ASCII (0 = 48 ASCII)
+int validateInput(char input, int argc){
+	
+	if (argc != 2)
+		return 0;
+	int result = 0;
+	if((input - 48) >=0 && (input - 48 ) <=9 )
+		result = 1;
+	return result;
+}//so far this validates the first char of the input, also needs to validate that there is one
+//input
+
+void* say_hello(void* data)
+{
+    char *str;
+    str = (char*)data;
+ 
+        printf("%s\n",str);
+
+}
+
+void* SimpleThread (int which){
 
 	int num, val;
 	for(num = 0; num < 20; num++){
